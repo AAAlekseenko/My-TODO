66d0db445dac9c5fc1c7e54c80d5ad1a6d0bca73
Andrey Alekseenko <altgulkaim@gmail.com>
2020-12-25 19:45:23 +0300

    feat add localStorage

--- src/components/task-list/TaskList.js
+++ src/components/task-list/TaskList.js
@@ -8,12 +8,35 @@ class TaskList extends React.Component {
         super(props);
         this.state = {
             items: [],
-            deleteItems: []
         }    
     }
-    
+    componentDidMount() {
+        this.parseLocalStor();
+    }
+
+    saveLocalStor = (items) => { 
+        localStorage.setItem('todo-list', JSON.stringify(items))
+        
+        
+   }
+
+   parseLocalStor = () => {
+    try {
+        let items = localStorage.getItem('todo-list');
+        if(!items) return; 
+        items = JSON.parse(items);
+        if(items.length) this.setState({items});
+
+    } catch (e) {
+        return alert(Error);
+    }
+   }
+ 
+
     handleItems = (item) => {
-        this.setState({items: [...this.state.items, item]})
+        const items = [...this.state.items, item]
+        this.setState({items})
+        this.saveLocalStor(items);
     }
 
     handleChangeItem = (item) => {
@@ -23,18 +46,23 @@ class TaskList extends React.Component {
             }
             return elem;
         });
+
         this.setState({items});
+        this.saveLocalStor(items);
     }
 
     handleDeleteItem = (id) => {
         const items = this.state.items.filter(item => item.id !== id);
         this.setState({items});
+        this.saveLocalStor(items);
     }
 
     handleDelAllCheckItems = () => {
         const items = this.state.items.filter(item => item.checked !== true);
         this.setState({items});
+        this.saveLocalStor(items);
     }
+
 
 
     render() {
